45bac62473acba1fcac39a1d32e2523a525d10f8
K1A2 <jckim0307@gmail.com>
2017-11-29 23:33:53 +0900

    SplashActivity, MainActivity추가

--- app/src/main/java/unitor/uni/k1a2/unitor2/activitys/MainActivity.java
+++ app/src/main/java/unitor/uni/k1a2/unitor2/activitys/MainActivity.java
@@ -1,8 +1,21 @@
 package unitor.uni.k1a2.unitor2.activitys;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+import unitor.uni.k1a2.unitor2.R;
+
 /**
  * Created by jckim on 2017-11-29.
  */
 
-public class MainActivity {
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+    }
 }
