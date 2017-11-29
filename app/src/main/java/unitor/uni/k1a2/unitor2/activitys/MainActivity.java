d85bc029f22ba3a1a63664a039b8fb82f6f03e1d
K1A2 <jckim0307@gmail.com>
2017-11-30 00:07:46 +0900

    MainActivity 살짝 수정

--- app/src/main/java/unitor/uni/k1a2/unitor2/activitys/MainActivity.java
+++ app/src/main/java/unitor/uni/k1a2/unitor2/activitys/MainActivity.java
@@ -17,5 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
     }
-}//
+}
