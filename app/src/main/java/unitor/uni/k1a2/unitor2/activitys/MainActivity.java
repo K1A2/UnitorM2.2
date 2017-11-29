d7351f02ece954440687291594d620d79ec92fc1
K1A2 <jckim0307@gmail.com>
2017-11-29 23:29:11 +0900

    SplashActivity, MainActivity추가

---
+++ app/src/main/java/unitor/uni/k1a2/unitor2/activitys/MainActivity.java
@@ -0,0 +1,8 @@
+package unitor.uni.k1a2.unitor2.activitys;
+
+/**
+ * Created by jckim on 2017-11-29.
+ */
+
+public class MainActivity {
+}
--- app/src/main/java/unitor/uni/k1a2/unitor2/activitys/SplashActivity.java
+++ app/src/main/java/unitor/uni/k1a2/unitor2/activitys/SplashActivity.java
@@ -1,7 +1,18 @@
 package unitor.uni.k1a2.unitor2.activitys;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 import unitor.uni.k1a2.unitor2.R;
 
@@ -15,7 +26,69 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            int permisionRequest = ContextCompat.checkSelfPermission(SplashActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            int permisionRequest2 = ContextCompat.checkSelfPermission(SplashActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+            if (permisionRequest == PackageManager.PERMISSION_DENIED||permisionRequest2 == PackageManager.PERMISSION_DENIED)
+                if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)||shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    AlertDialog.Builder permissioCheck = new AlertDialog.Builder(SplashActivity.this);
+                    permissioCheck.setTitle(getString(R.string.per_re_title))
+                            .setMessage(getString(R.string.per_re_content))
+                            .setCancelable(false)
+                            .setPositiveButton(getString(R.string.agree), new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                        ActivityCompat.requestPermissions(SplashActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1000);
+                                    }
+                                }
+                            })
+                            .setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Toast.makeText(SplashActivity.this, getString(R.string.unable_save), Toast.LENGTH_SHORT).show();
+                                    startMain();
+                                }
+                            })
+                            .create()
+                            .show();
+                } else {
+                    ActivityCompat.requestPermissions(SplashActivity.this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+                } else {
+                startMain();
+            }
+        } else {
+            startMain();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 100:
+
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startMain();
+                } else {
+                    Toast.makeText(SplashActivity.this, R.string.unable_save, Toast.LENGTH_SHORT).show();//
+                    startMain();
+                }
+                return;
+        }
+    }
+    private void startMain() {
+        Handler h = new Handler();
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
     }
 }
