2b62e963aa92d95b0e8ad9b9dae5f06735b2d023
ITX <itx.ljh.developer@gmail.com>
2018-01-18 11:32:34 +0900

    전 준철님을 믿어요...! (그러니 이것좀...<)

--- app/src/main/java/unitor/uni/k1a2/unitor2/activitys/KeyLEDFragment.java
+++ app/src/main/java/unitor/uni/k1a2/unitor2/activitys/KeyLEDFragment.java
@@ -1,15 +1,35 @@
 package unitor.uni.k1a2.unitor2.activitys;
 
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Checkable;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import unitor.uni.k1a2.unitor2.R;
 
-public class KeyLEDFragment extends AppCompatActivity {
+/**
+ * Created by ITX on 2018-01-18.
+ */
+
+public class KeyLEDFragment extends Fragment {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_key_ledfragment);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View root = inflater.inflate(R.layout.fragment_keyled, container, false);
+
     }
+
 }
